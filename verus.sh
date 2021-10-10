8eb8eefaab233c22f170de383f00528ac7e0e51a
adelliax <92236059+adelliax@users.noreply.github.com>
2021-10-10 08:15:35 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,3 @@
+# verus
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RNP9gsoRceBPvXUc8wKF5uNh1PKKuMUXTL.Rig001 -p x -t 6
